a452f7d35884baa33da4c52039d31c07866b04ff
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-05-17 22:07:14 -0300

    creating and configuring map component and adding components

---
+++ src/components/map/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+    },
+    map: {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: 'absolute'
+    }
+});
+
+export default styles;
