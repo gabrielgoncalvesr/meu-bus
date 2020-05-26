87371ac6dd2cf1751943e0d490ba481dbb24188c
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-05-26 00:53:16 -0300

    fixing fetch socket

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 const http = require("http");
 const fetch = require("node-fetch");
-
 const socketIO = require("socket.io");
 
 const server = http.createServer(app);
@@ -9,42 +8,32 @@ const io = socketIO(server);
 
 let interval;
 
-
 io.on("connection", (socket) => {
     console.log("New client connected");
+
+    socket.on("disconnect", () => {
+        console.log("client disconnected");
+    });
+
     if (interval) {
         clearInterval(interval);
     }
 
     socket.on("/position", ({ code }) => {
-        console.log("code:" + code);
-        interval = setInterval(() => emitBusPositionMessage(socket, code), 1000);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("client disconnected");
+        interval = setInterval(() => {
+            emitBusPositionMessage(socket, code)
+        }, 2000);
     });
 });
 
-
-const emitBusPositionMessage = (socket, code) => {
-    fetch(`http://${process.env.SERVER_HOST}:${process.env.PORT}/sptrans/search/position?code=${code}`)
-        .then(function (response) {
-            if (response.status >= 400) {
-                throw new Error("Bad request response from server");
-            }
-            console.log("AQ", response.body)
-
-        }).then(function (body) {
-           //console.log(body)
-           console.log("AQ2", body)
+const emitBusPositionMessage = async (socket, code) => {
+    const results = await fetch(`http://${process.env.SERVER_HOST}:${process.env.PORT}/sptrans/search/position?code=${code}`)
+        .then(res => res.json())
+        .then(json => {
+            return json;
         });
 
-    //console.log("ENTROU")
-
-    socket.emit("/position", "teste");
+    socket.emit("/position", results);
 };
-
-
 
 module.exports = server;
