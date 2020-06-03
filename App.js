4cbcfaab0a603a902b5ba2c0e5aeff6a8aa98456
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-06-02 22:40:36 -0300

    removing unused code

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import { YellowBox } from 'react-native';
-
 import Routes from './src/routes';
-
-YellowBox.ignoreWarnings([
-    'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
-]);
-
-console.ignoredYellowBox = ['Remote debugger'];
 
 const App = () => {
 	return (
